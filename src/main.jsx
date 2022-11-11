54a26fb5869f303f80f6c4f13b62cad1dccf6627
guillermosifu <sifuentes1201@gmail.com>
2022-11-10 21:23:01 -0500

    github

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Git from './gitSearch/Git'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Git/>    
   </React.StrictMode>
 )
